7cb1dbe50f9d12a7be7af9ceb82d24a604815c0e
Mirco Kraenz <kraenzmirco@gmail.com>
2020-06-04 12:34:07 +0900

    ADD #149 reload on app state change if a day has passed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "react-native-gesture-handler";
 import { Provider as StoreProvider } from "react-redux";
+import AppStateChangedContainer from "./app-containers/AppStateChangedContainer";
 import NetInfoChangedContainer from "./app-containers/NetInfoChangedContainer";
 import "./localization/i18n";
 import store from "./state/store";
@@ -10,7 +11,9 @@ export default function App() {
     return (
         <StoreProvider store={store}>
             <NetInfoChangedContainer>
-                <ThemedApp />
+                <AppStateChangedContainer>
+                    <ThemedApp />
+                </AppStateChangedContainer>
             </NetInfoChangedContainer>
         </StoreProvider>
     );
